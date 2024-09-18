68c923e3030bde72dd5a08b5a4b27dbca2ff635c
desertsniper87 <torshobuet@gmail.com>
2024-09-18 15:18:22 +0600

    [master] [18Sep-15%] init

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+// somewhere in your initialization file
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
   });
+
+  app.use(cookieParser());
   await app.listen(3000);
 }
+
 bootstrap();
